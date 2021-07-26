e893efe5dcf40da361b1cd2539bcdafdbec659ab
Joashfeh <63983498+Joashfeh@users.noreply.github.com>
2021-07-26 21:59:05 +0800

    dynamic videos

---
+++ js/videoData.js
@@ -0,0 +1,26 @@
+var dataList = [
+	{
+		name: "Everything Will Freeze FC",
+		url: "https://www.youtube.com/embed/adul7_QbLuk",
+	},
+	{
+		name: "Sunglow - SG Top Play",
+		url: "https://streamable.com/e/ygopc9",
+	},
+	{
+		name: "OWC2019 - Singapore vs Denmark",
+		url: "https://www.youtube.com/embed/Xz3OlViaV2A",
+	},
+	{
+		name: "Top 10 osu!SG Players 2019",
+		url: "https://www.youtube.com/embed/X02jR2wPfkU",
+	},
+	{
+		name: "Gotoubun FC",
+		url: "https://www.youtube.com/embed/70xj5yT0Mpo",
+	},
+	{
+		name: "OWC2020 - Singapore vs Japan",
+		url: "https://streamable.com/e/ygopc9",
+	},
+];
